142ae749038b94223b0009ed9ea2bf52a4ed9a4f
skoflok <skoflok@gmail.com>
2022-06-03 15:37:41 +0400

    remove all circle

--- box.go
+++ box.go
@@ -112,7 +112,7 @@ func (b *box) checkIndex(index int) (bool, error) {
 	return true, nil
 }
 
-func isCircle(t interface{}) bool {
+func isCircle(t *Shape) bool {
 	switch t.(type) {
 	case Circle:
 		return true
